c0ae0c27b8fa923b452a4254191be32f6e7a45e8
liaoworking <liaoworking@gmail.com>
2019-04-30 14:59:51 +0800

    适配swift 5

--- GHMoyaNetWorkTest/GHMoyaNetWorkTest/Router/NetworkManager.swift
+++ GHMoyaNetWorkTest/GHMoyaNetWorkTest/Router/NetworkManager.swift
@@ -218,26 +218,3 @@ var isNetworkConnect: Bool {
         return network?.isReachable ?? true //无返回就默认网络已连接
     }
 }
-
-/// Demo中并未使用，以后如果有数组转json可以用这个。
-struct JSONArrayEncoding: ParameterEncoding {
-    static let `default` = JSONArrayEncoding()
-    
-    func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
-        var request = try urlRequest.asURLRequest()
-        
-        guard let json = parameters?["jsonArray"] else {
-            return request
-        }
-        
-        let data = try JSONSerialization.data(withJSONObject: json, options: [])
-        
-        if request.value(forHTTPHeaderField: "Content-Type") == nil {
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        }
-        
-        request.httpBody = data
-        
-        return request
-    }
-}
