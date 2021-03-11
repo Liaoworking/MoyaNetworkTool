c0383e02fd096d223ad79af5c1ec4bcc159a1eb8
liaoworking <liaoworking@gmail.com>
2021-03-11 19:19:18 +0800

    feat: 网络请求封装代码重构。 使网络请求更Swifty

--- GHMoyaNetWorkTest/GHMoyaNetWorkTest/ViewController.swift
+++ GHMoyaNetWorkTest/GHMoyaNetWorkTest/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        testZhiHuDailyAPI()///演示moya+ObjectMapper   大部分业务场景使用的是这个调用。
+        testZhiHuDailyAPI()///演示moya+ObjectMapper   大部分业务场景使用的是这种形式调用。
         testAPI()//调用这个方法只是演示post请求 接口是调不通的
         multiServiceModule() // 多业务模块使用
     }
@@ -39,16 +39,16 @@ class ViewController: UIViewController {
 
     }
     
-    /// 基本使用
+    /// 基本使用 网络请求只是参考 具体接口调不通
     func testAPI() {
-        var paraDict: [String:Any] = Dictionary()
+        var paraDict: [String:Any] = [:]
         paraDict["app_type_"] = "1"
         paraDict["app_version_no_"] = "1.0.1"
         paraDict["platform_type_"] = "2"
         paraDict["ver_code_value_"] = nil
         
-        NetWorkRequest(API.updateAPi(parameters: paraDict), modelType: [ZhihuItemModel].self) { (zhihuModels, responseModel) in
-            print("服务器传回来的Array数据模型个数\(zhihuModels.count)")
+        NetWorkRequest(API.updateAPi(parameters: paraDict), modelType: ZhihuItemModel.self) { (zhihuModel, responseModel) in
+            print("服务器传回来的Array数据模型\(zhihuModel)")
         } failureCallback: { (responseModel) in
             print("网络请求失败 包括服务器错误和网络异常\(responseModel.code)__\(responseModel.message)")
         }
@@ -61,13 +61,11 @@ class ViewController: UIViewController {
         para["juid"] = "id"
         para["file_type_"] = "head"
         
-        let imageData = UIImage().jpegData(compressionQuality: 0.3) //把图片转换成data
-        NetWorkRequest(API.uploadHeadImage(parameters: para, imageDate: imageData!),modelType: [ZhihuItemModel].self, successCallback: {_,_ in print("图片上传成功")}, failureCallback: nil)
-
+        guard let imageData = UIImage().jpegData(compressionQuality: 0.3) else { return }//把图片转换成data
+        NetWorkRequest(API.uploadHeadImage(parameters: para, imageDate: imageData),modelType: [ZhihuItemModel].self, successCallback: {_,_ in print("图片上传成功")}, failureCallback: nil)
     }
     
-    
-    /// 多业务模块时候的网络请求
+    /// 不同的业务模块的网络请求
     func multiServiceModule() {
         // 登录模块的网络请求
         NetWorkRequest(APILogin.login, modelType: [ZhihuItemModel].self, successCallback: {_,_ in print("登录模块的api")}, failureCallback: nil)
@@ -75,8 +73,6 @@ class ViewController: UIViewController {
         NetWorkRequest(APIUser.getInfo, modelType: ZhihuItemModel.self, successCallback: {_,_ in print("用户模块的api")}, failureCallback: nil)
         // 商品列表获取
         NetWorkRequest(APIShops.getGoods, modelType: [ZhihuItemModel].self, successCallback: {_,_ in print("商品模块的api")}, failureCallback: nil)
-
-        
     }
     
     
